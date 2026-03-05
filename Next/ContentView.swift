8706b9bb3681020d4386183cd4844158bca62500
Colin Sweeney <colin.evan.sweeney@gmail.com>
2026-03-04 18:14:53 -0800

    Initial Commit

---
+++ Next/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Next
+//
+//  Created by Colin Sweeney2 on 3/4/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Next/NextApp.swift
@@ -0,0 +1,17 @@
+//
+//  NextApp.swift
+//  Next
+//
+//  Created by Colin Sweeney2 on 3/4/26.
+//
+
+import SwiftUI
+
+@main
+struct NextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
